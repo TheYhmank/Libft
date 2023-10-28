57af6db42f60d99ec3d5ac6473d4a89ed57e4cd4
Alibi <ayermeko@student.42prague.com>
2023-10-28 16:19:12 +0200

    ft_substr

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ayermeko <ayermeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 16:58:06 by ayermeko          #+#    #+#             */
-/*   Updated: 2023/10/24 23:05:53 by ayermeko         ###   ########.fr       */
+/*   Updated: 2023/10/26 17:31:33 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,3 +22,4 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	ft_bzero(rtn, nmemb);
 	return (rtn);
 }
+// u
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ayermeko <ayermeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 16:35:09 by ayermeko          #+#    #+#             */
-/*   Updated: 2023/10/23 16:54:52 by ayermeko         ###   ########.fr       */
+/*   Updated: 2023/10/26 18:18:01 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,35 @@
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-    
+    char *substr;
+    size_t i;
+    size_t len_s;
+
+    if(!s)
+        return (NULL);
+    len_s = ft_strlen(s);
+    if (start >= len_s)
+        return (ft_strdup("")); // NULL
+    if (len_s < start + len)
+        len = len_s - start;
+    substr = (char *)malloc((len + 1)); // test sizeof
+    if (!substr)
+        return (NULL);
+    i = 0;
+    while (i < len && s[start + i])
+    {
+        substr[i] = s[start + i];
+        i++;
+    }
+    substr[i] = '\0';
+    return (substr);
 }
 
 int main(void)
 {
-    
+    char str[] = "klsdjfs";
+    char *res = ft_substr(str, 2, 2);
+    printf("%s", res);
+    //free(res);
+    return (0);
 }
